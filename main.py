8f7d6500d0fd8bb50aa48c4d27172a8815833679
Nikita <wolf24ru@gmail.com>
2022-02-22 06:19:56 +0700

    Compl

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sqlite3
 import aiosmtplib
 
 from email.message import EmailMessage
+from more_itertools import chunked
+
+LIMITATION = 20
 
 
 class Person:
@@ -10,12 +13,12 @@ class Person:
         self.name = name
         self.surname = surname
         self.email = email
-        self.email_masg = f'''Уважаемый {self.name}!
+        self.email_msg = f'''Уважаемый {self.name}!
         Спасибо, что пользуетесь нашим сервисом объявлений.'''
 
 
 # генератор людей
-def gen_pipel():
+def gen_peoples():
     con = sqlite3.connect('contacts.db')
     cur = con.cursor()
     sql = 'select first_name, last_name, email from contacts'
@@ -31,43 +34,16 @@ async def send_email(email, msg):
     message["To"] = email
     message["Subject"] = "Hello World!"
     message.set_content(msg)
-    # print(email)
-    await aiosmtplib.send(message, hostname="127.0.0.1", port=1025)
 
-
-async def email_send():
-    email_tasks = [asyncio.create_task(send_email(person.email, person.email_masg))
-                   for person in gen_pipel()]
-    print(email_tasks)
-    await asyncio.wait(email_tasks)
-    # email_tasks = [asyncio.create_task(send_email(person.email, person.email_masg))
-    #                for person in gen_pipel()]
-    # email_send = asyncio.gather(*email_tasks)
-    # # for _email in email_send:
-    # #     yield _email
+    await aiosmtplib.send(message, port=1025)
 
 
 async def main():
-    email_tasks = [asyncio.create_task(send_email(person.email, person.email_masg))
-                   for person in gen_pipel()]
-    print(email_tasks)
-    await asyncio.gather(*email_tasks)
+    for persons in chunked(gen_peoples(), 20):
+        email_tasks = [asyncio.create_task(send_email(person.email, person.email_msg))
+                       for person in persons]
+        await asyncio.gather(*email_tasks)
 
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(email_send())
-    # loop.close()
-
-# генератор людей для отправки
 if __name__ == '__main__':
     asyncio.run(main())
-    # loop = asyncio.get_event_loop()
-    # loop = asyncio.new_event_loop()
-    # asyncio.set_event_loop(loop)
-    # email_tasks = [asyncio.create_task(send_email(person.email, person.email_masg))
-    #                for person in gen_pipel()]
-    # email_tasks = [send_email(person.email, person.email_masg)
-    #                for person in gen_pipel()]
 
-    # loop.run_until_complete(asyncio.gather(*email_tasks))
-    # loop.run_until_complete(email_send())
-    # loop.close()
